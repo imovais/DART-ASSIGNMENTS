f14f60df23c3366f81008b1babf5bc3c3d72860a
muhammadovaiskhan <ovais.k1991@gmail.com>
2023-03-06 13:59:59 +0500

    First Commit

---
+++ Assignment-5.dart
@@ -0,0 +1,11 @@
+/*
+Q1:  Map<String, double> expenses = {
+  'sun': 3000.0,
+  'mon': 3000.0,
+  'tue': 3234.0,
+};
+Check if "fri" exist in expanses; if exist change
+ it's value to 5000.0 otherwise add 'fri' to expenses
+  and set its value to 5000.0 then print expenses.
+*/
+
